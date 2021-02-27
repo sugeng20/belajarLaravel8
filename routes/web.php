b9a3d3f2243607c001b18ce66c0833bb226aaf1f
sugeng20 <sugeng.wanantara@gmail.com>
2021-02-27 15:58:51 +0700

    Login Page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
+
+Route::get('/login', [AuthController::class, 'index']);
